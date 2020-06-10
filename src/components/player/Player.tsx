bf0fcb38ba8526e7c296a0b97c6bc78595fdc7b2
roconnorr <roryoconnor@outlook.co.nz>
2020-06-10 23:42:51 +1200

    test video tag

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -5,7 +5,10 @@ import * as HPlayer from 'react-howler-player';
 class Player extends React.Component {
   render(): ReactElement {
     return (
-      <HPlayer src={[`http://localhost:3005/tracks/${1}`]} isDark={true} />
+      <video width="100%" height="480" controls>
+        <source src={`http://localhost:3005/tracks/${1}`} type="audio/mp3" />
+        Your browser does not support the video tag.
+      </video>
     );
   }
 }
