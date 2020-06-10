1de54d0c2f53b9f24031de8ad20e7a14973b3abe
Rory O'Connor <roryoconnor@outlook.co.nz>
2020-06-10 14:26:45 +1200

    Format server.js, add nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
-const fs = require("fs");
-const { Readable } = require("stream");
+const fs = require('fs');
+const { Readable } = require('stream');
 
-const express = require("express");
+const express = require('express');
 const trackRoute = express.Router();
 
 /**
  * Create Express server and routes
  */
 const app = express();
-app.use("/track", trackRoute);
+app.use('/track', trackRoute);
 
 /**
  * GET /tracks/:trackID
  */
-trackRoute.get("/:trackID", (req, res) => {
-  res.set("content-type", "audio/mp3");
-  res.set("accept-ranges", "bytes");
+trackRoute.get('/:trackID', (req, res) => {
+  res.set('content-type', 'audio/mp3');
+  res.set('accept-ranges', 'bytes');
 
   const fileStream = fs.createReadStream("./kaytestnada.mp3");
 
@@ -23,5 +23,5 @@ trackRoute.get("/:trackID", (req, res) => {
 });
 
 app.listen(3005, () => {
-  console.log("istening on port 3005");
+  console.log('Listening on port 3005');
 });
