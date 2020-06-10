e7592039abd85bd1fd03a16cd13abefe06d2aeed
Rory O'Connor <roryoconnor@outlook.co.nz>
2020-06-10 15:52:52 +1200

    Add db and music metadata packages, add temp hax song directory scanning

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,60 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
-const { Readable } = require('stream');
 
 const express = require('express');
 const trackRoute = express.Router();
+
+const db = require('better-sqlite3')('dev.db', { verbose: console.log });
+const mm = require('music-metadata');
+
+// create db schema
+const createTrackTable = db.prepare(
+  `
+  CREATE TABLE IF NOT EXISTS tracks (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    year TEXT NOT NULL,
+    path TEXT NOT NULL
+  );
+  `
+);
+
+createTrackTable.run();
+
+const insertTrack = db.prepare(
+  `INSERT INTO tracks (title, artist, year, path) VALUES (@title, @artist, @year, @path)`
+);
+
+// startup hax - read music dir and insert into the db
+// TODO - store last updated time for file in db, check if it has changed and only update if it has
+
+// get all file paths in music dir for parsing
+const filePaths = fs.readdirSync('./music');
+
+filePaths.forEach(path => {
+  const fullPath = `./music/${path}`;
+  mm.parseFile(fullPath)
+    .then(metadata => {
+      // console.log(util.inspect(metadata, { showHidden: false, depth: null }));
+      const { title, artist, year } = metadata.common;
+
+      console.log(title, artist, year);
+
+      insertTrack.run({ title, artist, year, path: fullPath });
+    })
+    .catch(err => {
+      console.error(err.message);
+    });
+});
 
 /**
  * Create Express server and routes
  */
 const app = express();
 app.use('/track', trackRoute);
+
+const getSong = db.prepare(`SELECT * FROM tracks where id = ?`);
 
 /**
  * GET /tracks/:trackID
@@ -17,7 +63,33 @@ trackRoute.get('/:trackID', (req, res) => {
   res.set('content-type', 'audio/mp3');
   res.set('accept-ranges', 'bytes');
 
-  const fileStream = fs.createReadStream("./kaytestnada.mp3");
+  const { trackID } = req.params;
+
+  console.log(req);
+
+  if (!trackID) {
+    res.status(404).send('Track not found!');
+  }
+
+  const song = getSong.get(trackID);
+
+  console.log(song);
+
+  const fileStream = fs.createReadStream(song.path);
+
+  fileStream.on('error', err => {
+    res.status(500).send(err);
+  });
+
+  fileStream.pipe(res);
+});
+
+//streamtest
+trackRoute.get('/test', (req, res) => {
+  res.set('content-type', 'audio/mp3');
+  res.set('accept-ranges', 'bytes');
+
+  const fileStream = fs.createReadStream('./kaytestnada.mp3');
 
   fileStream.on('error', err => {
     res.status(500).send(err);
