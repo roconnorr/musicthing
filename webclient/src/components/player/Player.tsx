c0159d6a3db79369f4a2e8d2fae955e5aa4f1a44
Rory O'Connor <roryoconnor@outlook.co.nz>
2020-07-24 13:30:56 +1200

    feat: replace <video> with <audio> and fix the styling for the player controls

--- webclient/src/components/player/Player.tsx
+++ webclient/src/components/player/Player.tsx
@@ -1,16 +1,17 @@
 import React, { ReactElement } from 'react';
-// @ts-ignore
-import * as HPlayer from 'react-howler-player';
 import { Box } from '@chakra-ui/core/dist';
 
 class Player extends React.Component {
   render(): ReactElement {
     return (
-      <Box w="100%">
-        <video controls>
-          <source src={`http://localhost:3005/tracks/${1}`} type="audio/mp3" />
-          Your browser does not support the video tag.
-        </video>
+      <Box w="100%" position="fixed" bottom="0">
+        <audio
+          style={{ width: '100%' }}
+          controls
+          src={`http://localhost:3005/tracks/${1}`}
+        >
+          Your browser does not support the audio element.
+        </audio>
       </Box>
     );
   }
